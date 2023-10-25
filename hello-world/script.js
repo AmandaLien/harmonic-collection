48ce72feb44768d4e2f168601219ee11995c1b3c
Amanda L <143529070+AmandaLien@users.noreply.github.com>
2023-10-24 23:02:10 -0400

    java intro

---
+++ hello-world/script.js
@@ -0,0 +1,32 @@
+//intro to java
+
+var hello="hello workd";
+
+var int = 9;
+
+var bool = true; //boolean
+
+console.log(hello) //console.log is display
+
+//arrays - ordered items
+
+var fruits = ["peaches", "mango"]
+
+console.log(fruits)
+console.log(fruits[0]) //item number
+
+//math.floor rounds to the nearest down number
+
+console.log(fruits[Math.random()*fruits.length]);
+
+
+//objects - unordered items
+
+var anotherObject = {}
+
+//var can be named as any except for reserved names
+
+
+
+
+
