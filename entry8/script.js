9dee46e6a09349d468ccc747ec251eb22d6e9118
Amanda L <143529070+AmandaLien@users.noreply.github.com>
2023-11-07 10:10:39 -0500

    fixed scroll button

--- entry8/script.js
+++ entry8/script.js
@@ -1,3 +1,4 @@
+
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
@@ -7,10 +8,11 @@ function scrollFunction() {
     } else {
         document.getElementById("top-button").style.display = "none";
     }
-   
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-     $('html, body').animate({scrollTop:0}, 'smooth');
+    //$('html, body').animate({scrollTop:0}, 'smooth');
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
